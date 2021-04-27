2c57a68c2d16e8319ac54750659d4a7701fc0d7f
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-04-27 20:06:26 -0300

    Updates

--- src/services/AirportService/airportService.js
+++
@@ -1,5 +0,0 @@
-export const getAirportsByTerm = (searchTerm) => {
-  return fetch(
-    `https://api.skypicker.com/locations?location_types=airport&term=${searchTerm}`
-  ).then((response) => response.json());
-};
