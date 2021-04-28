5db15fb058b6a4b3dcfaddc81ddc3ff9795b0f33
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-04-28 13:05:33 -0300

    cleaning up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,8 @@
 import { Search } from "../Search/Search";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import { useState } from "react";
 
 export const App = () => {
-  const [searchParamters, setSearchParameters] = useState({});
-
   return (
     <Container component="main" maxWidth="sm">
       <CssBaseline />{" "}
