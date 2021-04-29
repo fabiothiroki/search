ab71dd479f2a02f1d98d80ca12183967d3bccfde
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-04-29 08:10:44 -0300

    fix tests

--- src/components/AirportSelector/tests/AirPortSelector.test.js
+++ src/components/AirportSelector/tests/AirPortSelector.test.js
@@ -16,7 +16,7 @@ const renderComponent = () => {
   const queryClient = new QueryClient();
   const wrapper = (
     <QueryClientProvider client={queryClient}>
-      <AirportSelector inputLabel="For" name="airport" />
+      <AirportSelector inputLabel="For" name="airport" onChange={jest.fn()} />
     </QueryClientProvider>
   );
 
--- src/services/Flight/tests/flighService.test.js
+++ src/services/Flight/tests/flighService.test.js
@@ -14,11 +14,18 @@ describe("Flight service", () => {
       })
     );
 
-    const result = await getFlights({});
+    const result = await getFlights({
+      dateFrom: "28/04/2021",
+      dateTo: "28/04/2021",
+      fly_from: "BWI",
+      fly_to: "TLV",
+      return_from: "30/04/2021",
+      return_to: "30/04/2021",
+    });
     expect(result).toEqual(mockFlightResponse);
     expect(global.fetch).toHaveBeenCalledTimes(1);
     expect(global.fetch).toHaveBeenCalledWith(
-      "https://api.skypicker.com/flights?partner=skypicker"
+      "https://api.skypicker.com/flights?dateFrom=28%2F04%2F2021&dateTo=28%2F04%2F2021&fly_from=BWI&fly_to=TLV&return_from=30%2F04%2F2021&return_to=30%2F04%2F2021&partner=skypicker&limit=10"
     );
   });
 });
