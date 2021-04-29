060f5b7d09b0277bd1798e52ea1aefa9413c15b5
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-04-29 11:52:35 -0300

    fix tests

--- src/services/Flight/tests/flightResponseformatter.test.js
+++ src/services/Flight/tests/flightResponseformatter.test.js
@@ -6,21 +6,28 @@ test("should format expected data", () => {
 
   expect(result).toEqual([
     {
-      arrivalDate: "12/14/2020, 9:47:00 AM",
+      arrivalDate: new Date(
+        mockFlightResponse.aTimeUTC * 1000
+      ).toLocaleString(),
       cityFrom: "San Francisco",
       cityTo: "San Diego",
-      departureDate: "12/14/2020, 2:01:00 AM",
+      departureDate: new Date(
+        mockFlightResponse.dTimeUTC * 1000
+      ).toLocaleString(),
       flyFrom: "SFO",
       flyTo: "SAN",
       id: "20a108d748b20000d8b4b5f0_0|08d720a148b900007840d555_0",
       price: "88 EUR",
     },
     {
-      arrivalDate: "12/14/2020, 9:47:00 AM",
+      arrivalDate: new Date(
+        mockFlightResponse.aTimeUTC * 1000
+      ).toLocaleString(),
       cityFrom: "San Francisco",
       cityTo: "San Diego",
-      cityTo: "San Diego",
-      departureDate: "12/14/2020, 2:01:00 AM",
+      departureDate: new Date(
+        mockFlightResponse.dTimeUTC * 1000
+      ).toLocaleString(),
       flyFrom: "SFO",
       flyTo: "SAN",
       id: "20a108d748b40000cea23c4e_0|08d720a148b6000046c7988b_0",
