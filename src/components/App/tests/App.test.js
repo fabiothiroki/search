d13dbf5d1f5647bdc86fb88e54ae1fb791de074f
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-05-01 18:43:52 -0300

    progress bar test

--- src/components/App/tests/App.test.js
+++ src/components/App/tests/App.test.js
@@ -1,9 +1,11 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { QueryClientProvider, QueryClient } from "react-query";
 import mockAirportResponse from "../../../services/Airport/test/mockAirportReponse";
 import App from "../App";
+import FORM_FIELDS from "../../Search/constants";
 
 const server = setupServer(
   rest.get("https://api.skypicker.com/locations", (req, res, ctx) =>
@@ -31,5 +33,29 @@ test("Render form and results", async () => {
 
   await waitFor(() => {
     expect(screen.queryByText(/Find cheap flights/i)).toBeInTheDocument();
+  });
+});
+
+test("Search submit", async () => {
+  renderComponent();
+
+  userEvent.type(screen.getByRole("textbox", { name: "From" }), "prag");
+  userEvent.click(await screen.findByText(/prague/i));
+
+  userEvent.type(screen.getByRole("textbox", { name: "To" }), "prag");
+  userEvent.click(await screen.findByText(/prague/i));
+
+  fireEvent.change(screen.getByTestId(FORM_FIELDS.DEPARTURE_DATE), {
+    target: { value: "2020-05-13" },
+  });
+
+  fireEvent.change(screen.getByTestId(FORM_FIELDS.RETURN_DATE), {
+    target: { value: "2020-05-15" },
+  });
+
+  fireEvent.submit(screen.getByTestId("submitButton"));
+
+  await waitFor(() => {
+    expect(screen.getByRole("progressbar")).toBeInTheDocument();
   });
 });
